6eba3c4424322f5a216957a89b4c51749cff726f
Maxim Markov <webgips@gmail.com>
2017-02-11 16:38:22 +0300

    add gulp tasks, update readme

---
+++ gulp/paths/css.foundation.js
@@ -0,0 +1,5 @@
+'use strict';
+// Пути к  внешним плагинам и библиотекам css
+module.exports = [
+  './node_modules/normalize.css/normalize.css'
+];
---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,5 @@
+'use strict';
+// Пути к внешним плагинам и библиотекам javascript
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js'
+];
