8e6d806b8fc98659dd4de7410d3a5caf0a76af2b
Maxim Markov <webgips@gmail.com>
2017-02-25 02:46:51 +0300

    add parallax effect for index page

--- src/js/script.js
+++ src/js/script.js
@@ -115,3 +115,32 @@
 
         }
     }
+//paralax
+$(function() {
+    var paralaxContainer = $('.paralax'),
+        layers = paralaxContainer.find('.paralax__layer');
+    layers.map(function (key, value) {
+        var bottomPosition = ((window.innerHeight / 2) * (key / 100));
+        $(value).css({
+            'bottom': '-' + bottomPosition + 'px'
+        });
+    });
+    $(window).on('mousemove',function (e) {
+        var mouseX = (e.pageX),
+            mouseY = (e.pageY),
+            X = (window.innerWidth / 2 ) - mouseX,
+            Y = (window.innerHeight / 2 ) - mouseY;
+
+        layers.map(function (index, value){
+            var widthPosition = X * (index / 100 ),
+                heightPosition = Y * (index / 100 ),
+                bottomPosition = ((window.innerHeight / 2) * (index / 100));
+            // console.log(index);
+            // console.log(value);
+            $(value).css({
+                'bottom' : '-' + bottomPosition +'px',
+                'transform': 'translate3d(' + widthPosition + 'px, ' + heightPosition + 'px, 0)'
+            });
+        });
+    });
+});
