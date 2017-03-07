a23ce3b833ed868e39732a8b081ed0725b475f29
Maxim Markov <webgips@gmail.com>
2017-03-07 21:25:27 +0300

    add fancybox

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 // Пути к внешним плагинам и библиотекам javascript
 module.exports = [
-  './node_modules/jquery/dist/jquery.min.js'
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/fancybox/dist/js/jquery.fancybox.pack.js'
 ];
